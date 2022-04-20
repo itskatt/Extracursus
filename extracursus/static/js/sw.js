ce2ef8615dcf832d7eeb63411f7ee9fbbc4ca2f2
Itskatt <raph10@mail.com>
2022-04-21 00:04:43 +0200

    cache index and pdf viewer html

--- extracursus/static/js/sw.js
+++ extracursus/static/js/sw.js
@@ -1,5 +1,16 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js")
 
+// cache index page and pdf viewer
+workbox.routing.registerRoute(
+    ({ url }) =>
+        url.pathname == "/" ||
+        url.pathname.endsWith("viewer.html"),
+
+    new workbox.strategies.NetworkFirst({
+        cacheName: "non-custom-html"
+    })
+)
+
 // cache scripts and stylesheets
 workbox.routing.registerRoute(
     ({ request }) =>
