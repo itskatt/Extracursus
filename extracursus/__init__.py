519ff21d91a5f67e449e15f6a7c1fb93e914671a
Itskatt <raph10@mail.com>
2022-04-27 22:03:58 +0200

    bundle and minify assets with flask-assets

--- extracursus/__init__.py
+++ extracursus/__init__.py
@@ -7,6 +7,7 @@ from functools import lru_cache
 
 from flask import (Flask, abort, make_response, redirect, render_template,
                    request, send_file, send_from_directory, session)
+from flask_assets import Environment
 from flask_talisman import Talisman
 
 from .intra_client import IntraClient
@@ -18,6 +19,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "development key DO NOT USE OUTSIDE OF DEV")
 app.permanent_session_lifetime = timedelta(minutes=10) # session will last 10 minutes
 
+Environment(app)
 Talisman(app, content_security_policy=None)
 
 active_clients = {}
